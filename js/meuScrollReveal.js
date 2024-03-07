1a6640275b30a0934cbc8b0e4577fb6298136e80
Mateus <mateus@harpia.tech>
2024-03-07 12:58:30 -0300

    Add fullstack ao nome e também reset false no scrollreveal

--- js/meuScrollReveal.js
+++ js/meuScrollReveal.js
@@ -1,8 +1,7 @@
-
 function initScrollReveal() {
 
     ScrollReveal().reveal('.headline', {
-        reset: true,
+        reset: false, 
         duration: 1000,
         origin: 'right'
     });
@@ -10,14 +9,16 @@ function initScrollReveal() {
 
     if (window.innerWidth < 768) {
         ScrollReveal().reveal('.headline', {
-            reset: true,
+            reset: false, 
             duration: 1000,
             distance: '0',
             origin: 'bottom'
         });
     }
 
-    ScrollReveal().reveal('.rolagem1');
+    ScrollReveal().reveal('.rolagem1', {
+        reset: false 
+    });
 }
 
 document.addEventListener('DOMContentLoaded', function() {
