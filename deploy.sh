b16e459bacc31c317404bcf3cd9a9c8158e56fa0
Mateus Brandeburski <mateus.brandeburski92@gmail.com>
2025-01-03 22:45:09 -0300

    ajuste no piperline de deploy

--- deploy.sh
+++ deploy.sh
@@ -5,49 +5,63 @@ REMOTE_USER="mateusbrandeburski"
 REMOTE_HOST="192.168.0.10"
 REMOTE_PORT="52222"
 REMOTE_DIR="/home/mateusbrandeburski/portfolio-central"
+REMOTE_DIR_BACKUP="/home/mateusbrandeburski/backups/portfolio"
 
 # Perguntar se o usuário quer prosseguir
-read -p "Você tem certeza que deseja enviar os arquivos para o servidor? (S/n) [N]: " CONFIRMATION
+read -p "Você tem certeza que deseja enviar os arquivos para o servidor? (S/n) [N]: " CONFIRMATION </dev/tty
 CONFIRMATION=${CONFIRMATION:-N} # Define "N" como padrão
 
 if [[ "$CONFIRMATION" =~ ^[Ss]$ ]]; then
-  echo "Iniciando processo de deploy..."
-
-  # Comandos para executar no servidor após o deploy
-#   REMOTE_COMMANDS="
-#     echo 'Parando e removendo container antigo...';
-#     docker stop portfolio-central || echo 'Container portfolio-central não estava em execução.';
-#     docker rm portfolio-central || echo 'Container portfolio-central já foi removido.';
-    
-#     echo 'Construindo nova imagem...';
-#     cd $REMOTE_DIR && docker build -t portfolio-central:latest .;
-
-#     echo 'Executando novo container...';
-#     docker run -d -p 8080:80 --restart=always --name portfolio-central portfolio-central:latest;
-
-#     echo 'Removendo imagens e containers desnecessários...';
-#     docker rmi \$(docker images -f \"dangling=true\" -q) || echo 'Nenhuma imagem pendente para remover.';
-#     docker rm \$(docker ps -aq --filter \"status=exited\") || echo 'Nenhum container parado para remover.';
-
-#     echo 'Deploy concluído com sucesso!';
-#   "
-
-  # Sincroniza os arquivos com o servidor remoto
   echo "Enviando arquivos para o servidor remoto..."
   rsync -avz -e "ssh -p $REMOTE_PORT" --exclude ".git" ./ "$REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR"
 
   if [ $? -eq 0 ]; then
     echo "Arquivos enviados com sucesso!"
-    
-    # Executa os comandos no servidor remoto
+    echo "----------------XXXX---------------"
     echo "Executando comandos no servidor remoto..."
-    ssh -p $REMOTE_PORT "$REMOTE_USER@$REMOTE_HOST" "$REMOTE_COMMANDS"
+    
+    ssh -p $REMOTE_PORT "$REMOTE_USER@$REMOTE_HOST" bash -c "'
+    set -e
+
+    echo \"Navegando para o diretório do projeto...\"
+    cd \"$REMOTE_DIR\"
+    sleep 2
+
+    echo \"Salvando backup...\"
+    if [ -d \"$REMOTE_DIR\" ]; then
+        mkdir -p \"$REMOTE_DIR_BACKUP\"
+        mv \"$REMOTE_DIR\" \"$REMOTE_DIR_BACKUP/$(date +%Y%m%d_%H%M%S)\"
+    fi
+    sleep 2
+
+    echo \"Apagando arquivos do servidor...\"
+    rm -rf \"$REMOTE_DIR\"/* \"$REMOTE_DIR\"/.[!.]*
+    sleep 2
+
+    echo \"Parando e removendo container antigo...\"
+    docker stop portfolio-central || echo \"Container portfolio-central não estava em execução.\"
+    docker rm portfolio-central || echo \"Container portfolio-central já foi removido.\"
+    sleep 2
+
+    echo \"Construindo nova imagem...\"
+    docker build -t portfolio-central:latest .
+    sleep 2
+
+    echo \"Executando novo container...\"
+    docker run -d -p 8080:80 --restart=always --name portfolio-central portfolio-central:latest
+    sleep 2
+
+    echo \"Removendo imagens e containers desnecessários...\"
+    docker rmi \$(docker images -f \"dangling=true\" -q) || echo \"Nenhuma imagem pendente para remover.\"
+    docker rm \$(docker ps -aq --filter \"status=exited\") || echo \"Nenhum container parado para remover.\"
+    sleep 2
+
+    echo \"Deploy concluído com sucesso!\"
+    '"
+
   else
     echo "Erro ao enviar os arquivos para o servidor remoto."
   fi
 else
   echo "Deploy cancelado pelo usuário."
 fi
-
-
-
